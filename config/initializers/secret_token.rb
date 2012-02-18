3c9aab2ed599db6c8b2645b610cb6ccf4223533f
Robert <kc7rwx@gmail.com>
2012-02-18 18:00:26 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dogbone::Application.config.secret_token = '1d11759f710e1801a1145f532a2fa6975fe9791e636ecd104eaf11c338de76c1acd39455cd8b9c50ed0fd65f9d193591e0fb2227ae5d73571a2ab296b3fc6888'
